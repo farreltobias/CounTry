2b47faa0e0a6af8a6d796e10ea504ef9f0781c16
Guilherme Farrel de Souza <garcia.farrel@gmail.com>
2024-04-25 23:37:53 -0300

    feat(styles): :lipstick: add default styles

---
+++ src/styles/global.ts
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  button {
+    cursor: pointer;
+    border: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  /* ::-webkit-scrollbar-thumb {
+    background: #888; 
+  } */
+
+  label {
+    cursor: inherit;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-light']};
+    transition: box-shadow 0.1s;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme['gray-5']};
+    color: ${({ theme }) => theme['gray-1']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem Sora, sans-serif;
+    font-optical-sizing: auto;
+    font-style: normal;
+    line-height: 1.1;
+  }
+
+  @media (max-width: 768px) {
+    html {
+      font-size: 80%;
+    }
+  }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  white: '#fff',
+  black: '#000',
+
+  'gray-1': '#D9D9D9',
+  'gray-2': '#565E6D',
+  'gray-3': '#252C3A',
+  'gray-4': '#1D2330',
+  'gray-5': '#181D28',
+  'gray-6': '#161A24',
+
+  'green-light': '#ABF770',
+  'green-dark': '#5CAD1D',
+
+  'blue-light': '#70AFFF',
+  'blue-dark': '#3066AB',
+
+  'red-light': '#F77070',
+  'red-dark': '#AB3030',
+} as const
